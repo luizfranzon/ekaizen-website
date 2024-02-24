493d8d43aa3cc90ef8ae2c0bf4685a98b3950cf1
Luiz Franzon <luizfranzon05@hotmail.com>
2024-02-24 15:07:14 -0300

    `in progress`: add home component with phone and circle

--- src/app/pages/HomePage/home-page.component.ts
+++ src/app/pages/HomePage/home-page.component.ts
@@ -1,10 +1,13 @@
 import { Component } from "@angular/core";
 import { HeaderComponent } from "../../components/Header/header.component";
+import { HeroSectionComponent } from "./components/hero-section/hero-section.component";
+import { NgOptimizedImage } from "@angular/common";
 
 @Component({
   selector: "app-home",
   standalone: true,
   templateUrl: "./home-page.component.html",
-  imports: [HeaderComponent]
+  providers: [NgOptimizedImage],
+  imports: [HeaderComponent, HeroSectionComponent]
 })
 export class HomePageComponent { }
